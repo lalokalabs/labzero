abee950526607dbbc25ef07b13454f01fd033ff3
Ngazetungue <nmuheuez@gmail.com>
2022-11-16 02:49:59 +0200

    create index.html file

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/templatetags/my_tags.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/templatetags/my_tags.py
@@ -1,9 +1,16 @@
 from django import template
-
+import datetime
 # Create the register instance by initializing it with the Library instance.
 register = template.Library()
 
 """
+Syntax
+
 def my_tags(arg):
     return arg
 """
+# example
+# A function that returns the current date.
+@register.simple_tag(name='current_date')
+def current_date(format):
+    return datetime.datetime.now().strftime(format)
